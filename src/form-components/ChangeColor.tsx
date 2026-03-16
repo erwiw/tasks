751deb0ef158540f2630138c5305f3fb5bec03d5
Wyatt <erwiw@udel.edu>
2026-03-16 00:40:37 -0400

    Completed remaining problems.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,10 +1,50 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-// export function ChangeColor(): React.JSX.Element {
-//     return (
-//         <div>
-//             <h3>Change Color</h3>
-//         </div>
-//     );
-// }
-export {};
+export function ChangeColor(): React.JSX.Element {
+    const [chosenColor, changeColor] = useState("red");
+    const COLORS = [
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Indigo",
+        "Violet",
+        "Black",
+        "Grey",
+    ];
+
+    return (
+        <div>
+            <h3>Change Color</h3>
+
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    key={color}
+                    role="radio"
+                    inline
+                    value={color}
+                    name="response"
+                    onChange={(event) => {
+                        changeColor(event.target.value);
+                    }}
+                    id={"response-" + color}
+                    label={color}
+                    style={{ backgroundColor: color }}
+                    checked={chosenColor === color}
+                />
+            ))}
+
+            <div>
+                You have chosen{" "}
+                <div
+                    data-testid="colored-box"
+                    style={{ backgroundColor: chosenColor }}
+                >
+                    {chosenColor}
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,17 +1,32 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-// export function MultipleChoiceQuestion({
-//     options,
-//     expectedAnswer,
-// }: {
-//     options: string[];
-//     expectedAnswer: string;
-// }): React.JSX.Element {
-//     return (
-//         <div>
-//             <h3>Multiple Choice Question</h3>
-//         </div>
-//     );
-// }
+export function MultipleChoiceQuestion({
+    options,
+    expectedAnswer,
+}: {
+    options: string[];
+    expectedAnswer: string;
+}): React.JSX.Element {
+    const [answer, selectAnswer] = useState(options[0]);
 
-export {};
+    function chooseAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        selectAnswer(event.target.value);
+    }
+
+    return (
+        <div>
+            <h3>Multiple Choice Question</h3>
+
+            <Form.Group controlId="userEmotions">
+                <Form.Label>What is your answer?</Form.Label>
+                <Form.Select value={answer} onChange={chooseAnswer}>
+                    {options.map((ans: string) => (
+                        <option key={ans}>{ans}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {expectedAnswer === answer ? "✔️" : "❌"}
+        </div>
+    );
+}
